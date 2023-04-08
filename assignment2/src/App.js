4f3851aa64103267327a61ddec2a6b0dc3b6edac
asand <asand@iastate.edu>
2023-04-08 14:57:26 -0500

    Began setting up product catalog

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import React, {useState} from "react";
+import {Products} from "./Products";
+import {Categories} from "./Categories";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export const App = () => {
+  console.log("Step 1 : After reading file :");
+
+  const [ProductsCategory, setProductsCategory] = useState(Products);
+
+  return <div>
+    {ProductsCategory.map((product, index) => (
+      <div key={index} >
+        <img alt="Product Image" src={product.image} />
+      </div>
+    ))}
+
+  </div>
 }
-
-export default App;
