34b915343bd8d977e759acecac15990be33a4495
asand <asand@iastate.edu>
2023-04-08 20:59:05 -0500

    Began working on the form

--- assignment2/src/script.js
+++ assignment2/src/script.js
@@ -14,6 +14,21 @@ export const App = () => {
   const [cart, setCart] = useState([]);
   const [cartTotal, setCartTotal] = useState(0);
 
+  //Declaring and Initialising Variables used with the submission form
+  const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+  const form = document.getElementById('checkout-form');
+  const inputCard = document.querySelector('#inputCard');
+  const alertTrigger = document.getElementById('submit-btn');
+  const summaryCard = document.querySelector('.card');
+  const summaryList = document.querySelector('.card > ul');
+
+  //Declaring and Initialising the order object for submitting the order
+  var order = {
+    name: '',
+    email: '',
+    card: ''
+  }
+
   //Used to update the cart's total cost
   useEffect(() => {
     total();
@@ -131,6 +146,98 @@ export const App = () => {
     </div>
   }
 
+  let validate = function () {
+    let val = true;
+    let email = document.getElementById('inputEmail4')
+    let name = document.getElementById('inputName')
+    let card = document.getElementById('inputCard')
+    if (!email.value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{ 1, 3}\.[0 - 9]{ 1, 3}\.[0 - 9]{ 1, 3}\.[0 - 9]{ 1, 3}\]) | (([a - zA - Z\-0 - 9] +\.) +[a - zA - Z]{ 2,})) $ /
+    )) {
+      email.setAttribute("class", "form-control is-invalid");
+      val = false;
+    }
+    else {
+      email.setAttribute("class", "form-control is-valid");
+      order.email = email.value
+    }
+    if (name.value.length == 0) {
+      name.setAttribute("class", "form-control is-invalid")
+      val = false
+    }
+    else {
+      name.setAttribute("class", "form-control is-valid");
+      order.name = name.value
+    }
+    if (!card.value.match(/^[0-9]{4}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}$/)) {
+      card.setAttribute("class", "form-control is-invalid")
+      val = false
+    }
+    else {
+      card.setAttribute("class", "form-control is-valid");
+      order.card = card.value
+    }
+    if (val) {
+      form.classList.add("collapse")
+      for (const [key, value] of Object.entries(order)) {
+        summaryList.innerHTML += '<li class="list-group-item"> <b>' + `${key}` +
+          ': </b>' + `${value}` + '</li>'
+      }
+      summaryCard.classList.remove("collapse")
+      alertPlaceholder.innerHTML = ""
+      alert('<i class="bi-cart-check-fill"></i> You have made an order!',
+        'success')
+    }
+    return val;
+  }
+
+  const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      ` <div>${message}</div>`,
+      ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label = "Close" ></button > ',
+      '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+  }
+
+  function isNumeric (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  inputCard.addEventListener('input', event => {
+    if(!inputCard.value) {
+      return event.preventDefault();
+    }
+    else{
+      inputCard.value = inputCard.value.replace(/-/g, '');
+      let newVal = '';
+      for(var i = 0, nums = 0; i < inputCard.value.length; i++){
+        if(nums != 0 && nums % 4 == 0){
+          newVal += '-';
+        }
+
+        newVal += inputCard.value[i];
+        if(isNumeric(inputCard.value[i])){
+          nums++;
+        }
+      }
+      inputCard.value = newVal;
+    }
+  });
+
+  form.addEventListener('submit', event => {
+    //if (!form.checkValidity()) {
+    if (!validate()) {
+    alertPlaceholder.innerHTML = ''
+    alert('<i class="bi-exclamation-circle"></i> Something went wrong!','danger')
+    }
+    event.preventDefault()
+    event.stopPropagation()
+    //form.classList.add('was-validated')
+    }, false );
+
   //Returns the stuff to render
   return (
     <body>
@@ -146,7 +253,7 @@ export const App = () => {
             <div className="py-10">
               {/* Cart Button */}
               <button className="inline-block bg-amber-600 rounded-full px-3 py-1
-               text-sm font-semibold text-gray-700 mr-2 mt-2" onClick={handleShowHideCart}>View Cart</button>
+               text-sm font-semibold text-gray-700 mr-2 mt-2" onClick={handleShowHideCart}>View Cart and Checkout</button>
               {/* Search Bar */}
               <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
             focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
@@ -173,7 +280,7 @@ export const App = () => {
           <b>
             {/* Cart Button */}
             <button className="bg-amber-600 rounded-full px-3 py-1
-               text-sm font-semibold text-gray-700 mr-2 mt-2" onClick={handleShowHideCart}>View Catalog</button>
+               text-sm font-semibold text-gray-700 mr-2 mt-2" onClick={handleShowHideCart}>Return and Continue Shopping</button>
           </b>
           <div class="card">
             <div class="row">
@@ -200,6 +307,115 @@ export const App = () => {
                 </p>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8">
+              <h1>Order Form</h1>
+              <div id="liveAlertPlaceholder"></div>
+              <form class="row g-3" id="checkout-form">
+
+                {/* Full Name */}
+                <div class="col-md-6">
+                  <label for="inputName" class="form-label">Full Name</label>
+                  <input type="text" class="form-control" id="inputName" />
+                  <div class="valid-feedback">
+                    Looks good!
+                  </div>
+                  <div class="invalid-feedback">
+                    Must be like, "John Doe"
+                  </div>
+                </div>
+
+                {/* Email */}
+                <div class="col-md-6">
+                  <label for="inputEmail4" class="form-label">Email</label>
+                  <input type="email" class="form-control" id="inputEmail4" />
+                  <div class="valid-feedback">
+                    Looks good!
+                  </div>
+                  <div class="invalid-feedback">
+                    Must be like, "abc@xyz.efg"
+                  </div>
+                </div>
+
+                {/* Credit Card */}
+                <div class="col-12">
+                  <label for="inputCard" class="form-label">Card</label>
+                  <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1"><i class="bi-credit-card-fill"></i></span>
+                    <input type="text" id="inputCard" class="form-control" placeholder="XXXX-XXXX-XXXX-XXXX"
+                      aria-label="Username" aria-describedby="basic-addon1" />
+                    <div class="valid-feedback">
+                      Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                      Must be like, "7777-7777-7777-7777"
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-12">
+                  <label for="inputAddress" class="form-label">Address</label>
+                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+                </div>
+                <div class="col-12">
+                  <label for="inputAddress2" class="form-label">Address 2</label>
+                  <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                </div>
+                <div class="col-md-6">
+                  <label for="inputCity" class="form-label">City</label>
+                  <input type="text" class="form-control" id="inputCity" />
+                </div>
+                <div class="col-md-4">
+                  <label for="inputState" class="form-label">State</label>
+                  <select id="inputState" class="form-select">
+                    <option selected>Choose...</option>
+                  </select>
+                </div>
+                <div class="col-md-2">
+                  <label for="inputZip" class="form-label">Zip</label>
+                  <input type="text" class="form-control" id="inputZip" />
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck" />
+                    <label class="form-check-label" for="gridCheck">
+                      Check me out
+                    </label>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <button type="submit" class="btn btn-outline-success"> <i class="bi-bag-check"></i> Order</button>
+                </div>
+              </form>
+
+              <div class="card collapse" style={{ width: '18rem' }}>
+                <div class="card-body">
+                  <h5 class="card-title">Order summary</h5>
+                  <p class="card-text">Here is a summary of your order.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+
+                </ul>
+                <a href="" onclick="location.reload()" class="btn btn-secondary"> <i class="bi-arrow-left-circle"></i>
+                  Return</a>
+              </div>
+
+              <footer class="bd-footer py-4 py-md-5 mt-5 bg-light">
+                <div class="container py-4 py-md-5 px-4 px-md-3">
+                  <div class="row">
+                    <div class="col-lg-12 mb-3">
+                      <b>SE/Com-S 319</b> Javascript form validation.
+                    </div>
+
+                  </div>
+                </div>
+              </footer>
+            </div>
+            <div class="col-2"></div>
           </div>
         </div>
       </div>}
