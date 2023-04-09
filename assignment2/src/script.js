2bdd9f0d35975966226ce9a09fc3bc79b2241285
asand <asand@iastate.edu>
2023-04-08 19:24:34 -0500

    Improved functionality of the cart/site in general

--- assignment2/src/script.js
+++ assignment2/src/script.js
@@ -15,14 +15,14 @@ export const App = () => {
   const [cartTotal, setCartTotal] = useState(0);
 
   //Used to update the cart's total cost
-  useEffect(() =>{
+  useEffect(() => {
     total();
   }, [cart]);
 
   //Function to calculate the total value of the cart
   const total = () => {
     let totalVal = 0;
-    for(let i = 0; i < cart.length; i++){
+    for (let i = 0; i < cart.length; i++) {
       totalVal += cart[i].price;
     }
     setCartTotal(totalVal);
@@ -78,12 +78,12 @@ export const App = () => {
           <img class="img-fluid" src={el.image} />
         </div>
         <div class="col">
-          <div class="row text-muted">{el.title}</div>
-          <div class="row">{el.category}</div>
+          <div class="row">{el.title}</div>
+          <div class="row text-muted">{el.category}</div>
         </div>
         <div class="col">
-          <button type="button" variant="light" onClick={() => removeFromCart(el)} > - </button>{" "}
-          <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+          <button class="btn btn-outline-secondary" type="button" onClick={() => removeFromCart(el)} > - </button>{" "}
+          <button class="btn btn-outline-secondary" type="button" onClick={() => addToCart(el)}> + </button>
         </div>
         <div class="col">
           ${el.price} <span class="close">&#10005;</span>{howManyofThis(el.id)}
@@ -91,6 +91,45 @@ export const App = () => {
       </div>
     </div>
   ));
+
+  //Renders the products
+  const render_products = (ProductsCategory) => {
+    return <div className='category-section fixed'>
+      <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
+      <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{
+        maxHeight: '800px', overflowY:
+          'scroll'
+      }}>
+        {/* Loop Products */}
+        {ProductsCategory.map((product, index) => (
+          <div key={index} className="group relative shadow-lg" >
+            <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-60 lg:aspect-none">
+              <img
+                alt="Product Image"
+                src={product.image}
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="flex justify-between p-3">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href={product.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
+                  </a>
+                  <p>Tag - {product.category}</p>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
+              </div>
+              <p className="text-sm font-medium text-green-600">${product.price}</p>
+            </div>
+            <button type="button" onClick={() => removeFromCart(product)} > - </button>{" "}
+            <button type="button" onClick={() => addToCart(product)}> + </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  }
 
   //Returns the stuff to render
   return (
@@ -131,6 +170,11 @@ export const App = () => {
       {showCart && <div id='top_cart'>
         <div>
           STORE SE/ComS319
+          <b>
+            {/* Cart Button */}
+            <button className="bg-amber-600 rounded-full px-3 py-1
+               text-sm font-semibold text-gray-700 mr-2 mt-2" onClick={handleShowHideCart}>View Catalog</button>
+          </b>
           <div class="card">
             <div class="row">
               {/* HERE, IT IS THE SHOPING CART */}
@@ -161,41 +205,4 @@ export const App = () => {
       </div>}
     </body>
   );
-}
-
-//Renders the products
-const render_products = (ProductsCategory) => {
-  return <div className='category-section fixed'>
-    <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
-    <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{
-      maxHeight: '800px', overflowY:
-        'scroll'
-    }}>
-      {/* Loop Products */}
-      {ProductsCategory.map((product, index) => (
-        <div key={index} className="group relative shadow-lg" >
-          <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-            <img
-              alt="Product Image"
-              src={product.image}
-              className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-            />
-          </div>
-          <div className="flex justify-between p-3">
-            <div>
-              <h3 className="text-sm text-gray-700">
-                <a href={product.href}>
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
-                </a>
-                <p>Tag - {product.category}</p>
-              </h3>
-              <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
-            </div>
-            <p className="text-sm font-medium text-green-600">${product.price}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  </div>
 }
